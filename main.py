68871940debe785e0d88b1f75a46951dfa3d961e
GMVZ <go.vivanco@duocuc.cl>
2023-02-13 06:19:56 -0300

    VZcanner v.1.0

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import json
+import string
+
+
+
+class Scanner(object):
+    
+    
+    def __init__(self):
+        
+        super(Scanner, self).__init__()
+        
+        self.available_drives = ['%s:' % d for d in string.ascii_uppercase if os.path.exists('%s:' % d)]
+    
+    def init(self):
+        
+        cursor = {
+            'object' : {
+                'found' : {
+                    'drive' : None,
+                    'root'  : [],
+                    'dirs'  : [],
+                    'files' : []
+                }
+            }
+        }
+        
+        data = []
+        
+        for drive in self.available_drives:
+            
+            drive = os.path.normpath(drive)
+            
+            cursor['object']['found']['drive'] = drive
+            
+            for root, dirs, files in os.walk(drive):
+                
+                root = os.path.normpath(root)
+                
+                cursor['object']['found']['root'] = root
+                cursor['object']['found']['dirs'] = dirs
+                cursor['object']['found']['files'] = files
+                
+                
+                data.append(json.dumps(cursor, indent = 4, sort_keys = True))
+        
+        
+        with open('data.json', 'w') as data_file:
+            
+            for line in data:
+                line = str(line)
+                
+                data_file.write(line)
+                
+        data_file.close()
+
+
+if __name__ == '__main__':
+    Scanner().init()
